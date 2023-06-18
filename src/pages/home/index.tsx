69aebac7946e40b876ebad0f44d0bb30eb400e95
罗智毅 <luozhiyi@yaowutech.com>
2023-06-18 17:31:03 +0800

    chore: 优化示例

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,17 +1,20 @@
-import { Button, Color } from '@leslies/rc-component';
+import { Space, Button, Color } from '@leslies/rc-component';
+import './styles.scss';
 export default function Home() {
   return (
-    <div>
+    <Space direction="vertical">
       <h1>Home</h1>
-      <Button type="primary">primary</Button>
+      <Button type="primary" block>
+        primary
+      </Button>
       <Color
         data={[
-          { name: '--brand-1', value: '#1890ff' },
-          { name: '--brand-2', value: '#000000' },
-          { name: '--brand-3', value: '#000000' },
-          { name: '--brand-4', value: '#000000' },
+          { name: 'brand-1', value: '#1890ff' },
+          { name: 'brand-2', value: '#000000' },
+          { name: 'brand-3', value: '#000000' },
+          { name: 'brand-4', value: '#000000' },
         ]}
       />
-    </div>
+    </Space>
   );
 }
