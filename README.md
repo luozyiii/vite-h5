25a05c0bb7d0fbcb46a4a4a261d8190c4eaf82c4
罗智毅 <luozhiyi@yaowutech.com>
2023-06-21 11:30:43 +0800

    chore: css增加自适应

--- README.md
+++ README.md
@@ -7,3 +7,39 @@ npm cache clean --force
 
 npm install @leslies/rc-component@0.0.1 --registry http://81.71.98.176:4873:4873
 ```
+
+### 移动自适应方案（px => vw）
+
+#### 安装
+
+```bash
+npm install postcss postcss-px-to-viewport autoprefixer -D
+```
+
+#### 配置 vite.config.ts
+
+```js
+import autoprefixer from 'autoprefixer';
+import pxToViewport from 'postcss-px-to-viewport';
+
+{
+  ...,
+  css: {
+    postcss: {
+      plugins: [
+        pxToViewport({
+          viewportWidth: 375,
+          unitPrecision: 3,
+          viewportUnit: 'vw',
+          selectorBlackList: ['.ignore'],
+          minPixelValue: 1,
+          mediaQuery: false,
+          // 【注意】样式的适配在组件层不做，在工程做
+          exclude: /node_modules\/(?!@leslies\/rc-component)/,
+        }),
+        autoprefixer(),
+      ],
+    },
+  },
+}
+```
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import * as path from 'path';
 import react from '@vitejs/plugin-react';
+import autoprefixer from 'autoprefixer';
+import pxToViewport from 'postcss-px-to-viewport';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
@@ -9,5 +11,22 @@ export default defineConfig(() => {
       alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
     },
     plugins: [react()],
+    css: {
+      postcss: {
+        plugins: [
+          pxToViewport({
+            viewportWidth: 375,
+            unitPrecision: 3,
+            viewportUnit: 'vw',
+            selectorBlackList: ['.ignore'],
+            minPixelValue: 1,
+            mediaQuery: false,
+            // 【注意】样式的适配在组件层不做，在工程做
+            exclude: /node_modules\/(?!@leslies\/rc-component)/,
+          }),
+          autoprefixer(),
+        ],
+      },
+    },
   };
 });
